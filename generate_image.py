dc21c31f34c74fc5979a78e192501e672e4f19a0
Indra Coppens <115002315+Indra2003c@users.noreply.github.com>
2025-03-24 17:06:38 +0100

    easy

--- generate_image.py
+++ generate_image.py
@@ -1,8 +1,8 @@
 import array
  
  # PPM header
-width = 256
-height = 128
+width = 3
+height = 3
 maxval = 255
 ppm_header = f'P6 {width} {height} {maxval}\n'
  
@@ -10,14 +10,14 @@ ppm_header = f'P6 {width} {height} {maxval}\n'
 image = array.array('B', [0, 0, 255] * width * height)
  
 # Fill with red the rectangle with origin at (10, 10) and width x height = 50 x 80 pixels
-for y in range(10, 90):
-	for x in range(10, 60):
+for y in range(2, 2):  #50
+	for x in range(2, 2): #35
 		index = 3 * (y * width + x)
 		image[index] = 255           # red channel
 		image[index + 1] = 0         # green channel
 		image[index + 2] = 0         # blue channel
 
 # Save the PPM image as a binary file
-with open('blue_red_example.ppm', 'wb') as f:
+with open('easy.ppm', 'wb') as f:
 	f.write(bytearray(ppm_header, 'ascii'))
 	image.tofile(f)
